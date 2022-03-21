af13032080fa5cd22e7e4b3efd09637cbbfdc71c
bigbear <993296096@qq.com>
2022-03-21 20:26:54 +0800

    fastmemcpy

--- dynamic_test/main.c
+++ dynamic_test/main.c
@@ -4,20 +4,9 @@
 
 int main(void)
 {
-    uint32_t a = (12 << 24) | (34 << 16) | (56 << 8) | 78;
-    unsigned char* dd = ((unsigned char*)&a) + 4;
-    unsigned char* ss = ((unsigned char*)&a);
-
-
-    printf("a is : %d\n", dd[-1]);
-    printf("a is : %d\n", dd[-2]);
-    printf("a is : %d\n", dd[-3]);
-    printf("a is : %d\n", dd[-4]);
-
-    printf("a is : %d\n", ss[0]);
-    printf("a is : %d\n", ss[1]);
-    printf("a is : %d\n", ss[2]);
-    printf("a is : %d\n", ss[3]);
-    
+    size_t dst = 231;
+    // size_t padding = (16 - (((size_t)dst) & 15)) & 15;
+    size_t padding = (16 - (((size_t)dst) & 15)) & 15;
+    printf("padding is : %ld\n", padding);
     return 0;
 }
