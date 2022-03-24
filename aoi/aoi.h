067058878bdbf589b45eae710e5c17055f1d6343
bigbear <993296096@qq.com>
2022-03-25 02:04:55 +0800

    aoi

---
+++ aoi/aoi.h
@@ -0,0 +1,21 @@
+#ifndef __AOI_H
+#define __AOI_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef void *(*aoi_Alloc)(void *ud, void *ptr, size_t sz);
+typedef void(aoi_Callback)(void *ud, uint32_t watcher, uint32_t marker);
+
+struct aoi_space;
+
+struct aoi_space *aoi_create(aoi_Alloc alloc, void *ud);
+struct aoi_space *aoi_new();
+void aoi_release(struct aoi_space *);
+
+// w(athcer) m(arker) d(rop)
+void aoi_update(struct aoi_space *space, uint32_t id, const char *mode,
+				float pos[3]);
+void aoi_message(struct aoi_space *space, aoi_Callback cb, void *ud);
+
+#endif
--- dynamic_test/main.c
+++ dynamic_test/main.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
-#include <stdint.h>
 #include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-    size_t dst = 231;
-    // size_t padding = (16 - (((size_t)dst) & 15)) & 15;
-    size_t padding = (16 - (((size_t)dst) & 15)) & 15;
-    printf("padding is : %ld\n", padding);
-    return 0;
+typedef struct _list {
+	int node;
+	struct _list *next;
+} list;
+
+int
+main(void) {
+	// int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	// int *p0 = &arr[0];
+	// int *p1 = &arr[1];
+	// int *p9 = &arr[9];
+
+	// printf("p1 - p0 : %ld \n", ((char *)p1 - (char *)p0));
+	return 0;
 }
